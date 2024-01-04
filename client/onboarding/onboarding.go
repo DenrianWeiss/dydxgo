5dd00a5f6ae6cbd600c0a9d31602caa1e473d178
DenrianWeiss <42314109+DenrianWeiss@users.noreply.github.com>
2024-01-04 13:22:47 +0800

    feat(onboarding): add onboarding using signed data

--- client/onboarding/onboarding.go
+++ client/onboarding/onboarding.go
@@ -40,6 +40,15 @@ func (b *OnBoarding) DeriveStarkKey(ethereumAddress string) string {
 	return fmt.Sprintf("0x%s", privateKey.Text(16))
 }
 
+func (b *OnBoarding) DeriveStarkKeyFromSignature(signature []byte) string {
+	sha3 := solsha3.SoliditySHA3(fmt.Sprintf("0x%x", signature))
+	hashedSignature := hexutil.Encode(sha3)
+
+	privateKey, _ := new(big.Int).SetString(hashedSignature, 0)
+	privateKey = new(big.Int).Rsh(privateKey, 5)
+	return fmt.Sprintf("0x%s", privateKey.Text(16))
+}
+
 func (b *OnBoarding) GetDefaultApiCredentialsSignature() string {
 	message := b.CreateMessage(map[string]interface{}{"action": constants.OffChainOnboardingAction})
 	sig, err := b.SignMessage(message)
@@ -56,6 +65,35 @@ func (b *OnBoarding) RecoverDefaultApiCredentials(ethereumAddress string) *types
 	if err != nil {
 		log.Panic(err)
 	}
+	signature := fmt.Sprintf("0x%x", sig)
+	rHex := signature[2:66]
+	rInt, _ := big.NewInt(0).SetString(rHex, 16)
+
+	hashedRBytes := solsha3.SoliditySHA3([]string{"uint256"}, rInt.String())
+	secretBytes := hashedRBytes[:30]
+	sHex := signature[66:130]
+	sInt, _ := big.NewInt(0).SetString(sHex, 16)
+
+	hashedSBytes := solsha3.SoliditySHA3([]string{"uint256"}, sInt.String())
+	keyBytes := hashedSBytes[:16]
+	passphraseBytes := hashedSBytes[16:31]
+
+	keyHex := hex.EncodeToString(keyBytes)
+	keyUuid := strings.Join([]string{
+		keyHex[:8],
+		keyHex[8:12],
+		keyHex[12:16],
+		keyHex[16:20],
+		keyHex[20:],
+	}, "-")
+	return &types.ApiKeyCredentials{
+		Secret:     base64.URLEncoding.EncodeToString(secretBytes),
+		Key:        keyUuid,
+		Passphrase: base64.URLEncoding.EncodeToString(passphraseBytes),
+	}
+}
+
+func (b *OnBoarding) RecoverDefaultApiCredentialsFromSignature(sig []byte) *types.ApiKeyCredentials {
 	signature := fmt.Sprintf("0x%x", sig)
 	rHex := signature[2:66]
 	rInt, _ := big.NewInt(0).SetString(rHex, 16)
--- client/onboarding/request.go
+++ client/onboarding/request.go
@@ -31,3 +31,25 @@ func (b *OnBoarding) CreateAccount(ethereumAddress string) (*OnboardingResponse,
 	}
 	return result, privateKey, nil
 }
+
+func (b *OnBoarding) CreateAccountWithSignatures(derivedKey string, ethereumAddress string, onboardingSig string) (*OnboardingResponse, error) {
+	// Stark key
+	pkInt, _ := new(big.Int).SetString(derivedKey, 0)
+	publicKey := ec.DerivePublicKey(pkInt)
+	// Convert public key to what dydx's api expect.
+	pkx := fmt.Sprintf("%x", publicKey.X.Bytes())
+	pky := fmt.Sprintf("%x", publicKey.Y.Bytes())
+	data := map[string]string{
+		"starkKey":            pkx,
+		"starkKeyYCoordinate": pky,
+	}
+	res, err := b.post("onboarding", data, ethereumAddress, onboardingSig)
+	if err != nil {
+		return nil, err
+	}
+	result := &OnboardingResponse{}
+	if err = json.Unmarshal(res, result); err != nil {
+		return nil, errors.New("json parser error")
+	}
+	return result, nil
+}
